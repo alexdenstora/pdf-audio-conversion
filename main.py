6eed5a0f39989d87157a039c67793c92d9f9c53a
Alexander Sahlstrom <Alexander.Sahlstrom@gmail.com>
2025-12-17 16:04:47 -0800

    implemented convert_to_text() function and extract_file_name() function program converts a given pdf file to a text file

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pdfplumber
+import os
+
+def extract_file_name(file_path):
+    basename = os.path.basename(file_path)
+    name, extension = os.path.splitext(basename)
+    return name
+
+def convert_to_text(file_path):
+    name = extract_file_name(file_path)
+    extension = ".txt"
+    file_name = name + extension
+    with pdfplumber.open(file_path) as pdf, open(file_name, "w", encoding="utf-8") as f:
+        for page in pdf.pages:
+            text = page.extract_text(x_tolerance=2) # lowered tolerance for identifying spaces between characters
+            if text:
+                f.write(text)
 def main():
-    pass
+    pdf = "./Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf" # will change to user input
+    convert_to_text(pdf)
         
 
 if __name__ == "__main__":
